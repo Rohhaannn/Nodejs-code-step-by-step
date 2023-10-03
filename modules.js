2caf1c063747ae2b1f47df6ff2532fe46e27605c
CarryAllen <rohanshingade1095@gmail.com>
2023-10-03 10:09:54 +0530

    file system operations

--- modules.js
+++ modules.js
@@ -3,4 +3,12 @@ console.log('Rohan')
 
 //non-global module
 const fs = require ('fs')
-fs.writeFileSync("hello.txt", "this is testing file for non-global module");
+// fs.writeFileSync("hello.txt", "this is testing file for non-global module");
+console.log("->>",__dirname);
+console.log("->>",__filename);
+
+
+//another syntax
+
+const dt = require('fs').writeFileSync;
+dt("abs.txt", "this is another syntax")
