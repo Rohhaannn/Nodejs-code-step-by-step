4fd655771ece845de9d622aadc9527335c4226af
CarryAllen <rohanshingade1095@gmail.com>
2023-10-04 12:41:04 +0530

    v11- static API done

---
+++ API/data.js
@@ -0,0 +1,12 @@
+const data = [
+  {name:'Rohan', email:'rohan@gmail.com', contact:'464346343'},
+  {name:'Akshay', email:'akshay@gmail.com', contact:'464346343'},
+  {name:'pravin', email:'pravin@gmail.com', contact:'464346343'},
+  {name:'gullu', email:'gullu@gmail.com', contact:'464346343'},
+  {name:'tillu', email:'tillu@gmail.com', contact:'464346343'},
+  {name:'monkey', email:'monkey@gmail.com', contact:'464346343'},
+  {name:'zebra', email:'zebra@gmail.com', contact:'464346343'},
+  {name:'glitch', email:'glitch@gmail.com', contact:'464346343'},
+  {name:'saunsei', email:'saunsei@gmail.com', contact:'464346343'}
+]
+module.exports = data;
