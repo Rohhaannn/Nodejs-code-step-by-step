32806b0654cb02dfdaac0f24fef5188727c6cb25
CarryAllen <rohanshingade1095@gmail.com>
2023-10-04 19:54:36 +0530

    vdo13 - display files list

--- video13/index.js
+++ video13/index.js
@@ -2,11 +2,11 @@ const fs = require('fs');
 const path = require('path');
 const dirPath = path.join(__dirname, 'files');
 
-// console.log(dirPath);
+console.log(dirPath);
 
-// for (i=0; i<5; i++){
-//   fs.writeFileSync(dirPath+'/hello'+i+'.txt','a simple hello text file');
-// }
+for (i=0; i<5; i++){
+  fs.writeFileSync(dirPath+'/hello'+i+'.txt','a simple hello text file');
+}
 
 fs.readdir(dirPath, (err, files)=>{
   console.log(files); // will get all files in array
@@ -16,7 +16,7 @@ fs.readdir(dirPath, (err, files)=>{
 })
 
 
-/*
+/* Interview Question:
 
 can we access files from another drive using node ?
 No. we can get only files from folder which inside of that web server bcz for now from which folder we are using node that folder acts as web server. so node doesn't know what happens outside of that server
