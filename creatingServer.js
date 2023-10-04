ea8823ff49dc103d501c2647be090e95da4af472
CarryAllen <rohanshingade1095@gmail.com>
2023-10-04 11:38:26 +0530

    video 10 - Nodemon done

--- creatingServer.js
+++ creatingServer.js
@@ -20,4 +20,19 @@ const http = require ('http');
 
 // http.createServer(dataControl).listen(4500);
 
+// interview question
+// Node js is async language
+
+/*
+
+Async - 
+first command is run
+second command is run
+third cmnd has cmplex data which will take 3-4 mins to run node will run this cmnd but will not wait for outout.
+it will continue to run next cmnds. when 3rd cmnd gets run & got output, it will automatically get displayed
+4th cmnd
+5th cmnd
+
+*/
+
 
