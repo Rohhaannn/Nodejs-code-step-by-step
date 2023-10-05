76d5e4da45d3ce953297c2f721b5911d7a29bc49
CarryAllen <rohanshingade1095@gmail.com>
2023-10-05 19:30:22 +0530

    video15 - Asynchronous programming

---
+++ video15/index.js
@@ -0,0 +1,26 @@
+// console.log('start exe');
+
+// console.log('logic exe...');
+
+// setTimeout(()=>{
+//   console.log('logic exe...');
+// },2000)
+
+// console.log('complete exe');
+
+//=========================================================================
+
+//there is darwback in this
+//suppose:
+
+let a = 10;
+let b = 0;
+
+setTimeout(()=>{
+  b=20;
+  // console.log(b);
+},2000);
+
+console.log(a+b);
+
+/* will get output as 10, actual value we should've get is 30. when we assign a = 10 & b = 0, that statement got run first thats  we get answer as 10, but the setTimeout function runs after 2 seconds value of b gets updated but never gets considered in calculation */
