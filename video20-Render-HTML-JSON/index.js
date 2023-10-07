e9f0bff5200253a3f39705ad425c74bdacca53ba
CarryAllen <rohanshingade1095@gmail.com>
2023-10-07 17:31:11 +0530

    20- HTML & JSON data rendered & pages linked

--- video20-Render-HTML-JSON/index.js
+++ video20-Render-HTML-JSON/index.js
@@ -2,39 +2,74 @@ const express = require('express');
 const app = express();
 
 //from bard to confirm whether response has been send or not
-app.use((req, res, next) => {
-  if (!res.headersSent) {
-    next();
-  } else {
-    console.error('Cannot set headers after they are sent to the client');
-  }
-});
+// app.use((req, res, next) => {
+//   if (!res.headersSent) {
+//     next();
+//   } else {
+//     console.error('Cannot set headers after they are sent to the client');
+//   }
+// });
 
 //creating Simple Home page
 app.get('',(req, res)=>{
-  res.send('<h1>Welcome, to Home Page</h1>');
+  res.send(`
+
+  <a href="">Home</a>
+  <a href="/about">About</a>
+  <a href="/help">Help</a>
+  <a href="/other">other</a>
+
+  <br>
+  <h1>Welcome, to Home Page</h1>
+  
+  `);
 })
 
 //creating about page- Rendering HTML data
 app.get('/about',(req, res)=>{
   res.send(`
+      <a href="/">Home</a>
+      <a href="/about">About</a>
+      <a href="/help">Help</a>
+      <a href="/other">other</a>
+    
     <h1>Welcome, About US</h1>
-    <input type="text" placeholder="User name"/>
+    <br>
+    <input type="text" placeholder="User name" value="${req.query.name}" />
     <button> Click Me </button>
   `);
 })
 
 //creating help page - Rendering JSON data
 app.get('/help',(req, res)=>{
-  res.send({
-    name: 'Ronak',
-    email: 'ronak@test.com'
-  });
+  res.send(`
+  
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/help">Help</a>
+    <a href="/other">other</a>
+    <br>
+    <br>
+    
+  ,
+    {
+      name: 'Ronak',
+      email: 'ronak@test.com'
+    }`
+  );
 })
 
 //creating other page - Rendering multiple objects (JSON data) by creating an array
 app.get('/other',(req, res)=>{
-  res.send([
+  res.send(`
+
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/help">Help</a>
+    <a href="/other">other</a>
+    <br>
+    <br>
+  ,[
       {
         name: 'Ronak',
         email: 'ronak@test.com'
@@ -54,8 +89,8 @@ app.get('/other',(req, res)=>{
       {
         name: 'Dimple',
         email: 'Dimple@test.com'
-      },
-  ]);
+      }
+  ]`);
 })
 
 app.listen(8080);
