478334c164aaabaaf4f9e42fc1c9b6b07b3aa4f6
CarryAllen <rohanshingade1095@gmail.com>
2023-10-23 20:12:43 +0530

    28 - MongoDb basic commands

---
+++ video28-MongoDB-basic-1/index.js
@@ -0,0 +1,46 @@
+/* 
+
+MongoDb basic :
+
+MongoDB is a noSQL database
+The data stored in a collection
+Collection don't have row & columns
+Data is sorted in the form of object.
+
+===========================================================
+
+Commands:
+
+to display present databases:
+show dbs
+
+------------------------------------------------
+
+create new database:
+use newDatabaseName
+>switched to db newDatabaseName
+
+------------------------------------------------
+
+create a new collection: (in new created db):
+db.createCollection('collectionName')
+
+------------------------------------------------
+
+delete collection (drop)
+db.collectionName.drop()
+
+------------------------------------------------
+
+delete database:
+db.dropDatabase()
+
+------------------------------------------------
+
+use admin
+>switched to db admin
+admin>
+
+==================================================================
+
+*/
