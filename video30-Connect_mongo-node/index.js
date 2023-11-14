98ae09b2f3874b8e93a3c5655efced68f9bda40d
CarryAllen <rohanshingade1095@gmail.com>
2023-11-14 12:46:37 +0530

    connection done

--- video30-Connect_mongo-node/index.js
+++ video30-Connect_mongo-node/index.js
@@ -1,14 +1,15 @@
 const { MongoClient } = require("mongodb");
 // const MongoClient = require('mongodb').MongoClient; // => Both are same syntax. this is an old one. above this is modern syntax
-const url = "mongodb://localhost:27017";
+const url = 'mongodb://localhost:27017';
 const database = 'e-comm';
 const client = new MongoClient(url); 
 
 async function getData() {
+  
   let result = await client.connect();
   let db = result.db(database);
   let collection = db.collection('products');
-  let response = await collection.find({}).toArray();
+  let response = await collection.find({}).toArray()
   console.log(response);
 }
 
