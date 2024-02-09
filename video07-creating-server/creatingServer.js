36f648ee5cfadfcc21851dfc7953da2b68dcfc5a
Rohan <shingaderohan96@gmail.com>
2024-02-09 12:20:18 +0530

    video 7: creating basic server complete

--- video07-creating-server/creatingServer.js
+++ video07-creating-server/creatingServer.js
@@ -1,40 +1,42 @@
-// how t create a server
+//to create basic server it is required to import http module like below
+const http = require("http");
 
-const http = require ('http');
-
-// what is the use of http ?
-// handles the http request & response in node js
-
-//first syntax - recommended
-// http.createServer((req, res)=>{
-//   res.write("<h1> hello this is Rohan </h1>")
-//   res.end();
+//this is the first syntax (Recommended)
+// http.createServer((req, resp)=>{
+  
+//   resp.write("<h1> Hello this is Rohan </h1>");
+//   resp.end();
 // }).listen(4500);
 
+/* what does "req" do?
+  when you submit a form or send a parameter through  url, to access those inside node
 
-//Another syntax
-// const dataControl = (req, res) => {
-//   res.write("<h1> hello this is Rohan. This testing is with another syntax </h1>")
-//   res.end();
-// } 
-
-// http.createServer(dataControl).listen(4500);
-
-// interview question
-// Node js is async language
-
-
-
-/*
-
-Async - 
-first command is run
-second command is run
-third cmnd has cmplex data which will take 3-4 mins to run node will run this cmnd but will not wait for outout.
-it will continue to run next cmnds. when 3rd cmnd gets run & got output, it will automatically get displayed
-4th cmnd
-5th cmnd
-
+  what does res do ?
+  when we have to send response to the client
 */
 
 
+//Another syntax
+const dataControl = (req, res) => {
+  res.write("<h1> This is testing with another syntax </h1>");
+  res.end();
+}
+
+http.createServer(dataControl).listen(6500);
+
+//normal funtction
+// function test (a) {
+//   return a*10;
+// }
+//arrow function
+// const test = (a) => a*10;
+
+
+
+/* Interview Questions : 
+Q. how can we send function as parameter ?
+Q. what are arrow functions ?
+Q. what 'http' module do ?
+http module handles the request & response of the server in node
+
+*/
