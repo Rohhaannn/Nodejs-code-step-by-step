715b3ac9d91982eca05b5553ed75aa58580fdb36
Rohan <shingaderohan96@gmail.com>
2024-02-09 19:50:08 +0530

    Video 10 : nodemon installed & intrview question

---
+++ video10-Nodemon/index.js
@@ -0,0 +1,15 @@
+console.log('this is trial for nodemon package');
+console.log('I am using it for the first time');
+
+/*
+Interview Question:
+
+Q. Nodejs is Async lang or sync language ?
+=> Async language
+
+Explaintion:
+when we talk about threding then nodejs an single threaded language
+
+when we talk about sync or Async then nodejs is an Async language
+
+*/
