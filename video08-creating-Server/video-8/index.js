2ee8ef93939773b33489b27da2a37eb50593af10
Rohan <shingaderohan96@gmail.com>
2024-02-09 15:57:55 +0530

    video 8 complete

---
+++ video08-creating-Server/video-8/index.js
@@ -0,0 +1,23 @@
+const colors = require("colors");
+console.log("package".blue);
+console.log("Node Manager".green);
+console.log("Node Manager".bgBlue);
+
+/*interview question
+
+Q. what is package.json files
+=> it keeps detaila related to our project.
+  which details ?
+  => related to coding like project version, project name, git repo, comnds used, packages used
+
+Q.  How to make it ?
+
+Q.  How to install external package ?
+
+Q.  what is package-lock.json file
+=>  It keeps details of our packages  
+
+Q.  Node.js is single threaded or multithreaded ?
+=>  Node.js is single threaded. Completes one task then move to another.
+
+*/
