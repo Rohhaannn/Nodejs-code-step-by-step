0ebe75f7682a70567a1e753eb3f7885cb465f62f
Rohan <shingaderohan96@gmail.com>
2024-02-09 19:29:17 +0530

    video 9 interview question added

--- video09-challenge/chalk.js
+++ video09-challenge/chalk.js
@@ -1,3 +1,14 @@
 import chalk from 'chalk';
 
 console.log(chalk.blue('Hello world!'));
+
+/*
+Interview Question
+
+Q.  How to install node modules ?
+Q.  What happens if node modules folder is deleted ?
+Q.  Correct way to push the data on git ? OR shall we push node modules on git ?
+=>  No.
+
+
+ */
