457958d6d1324071c67b36c372cd122479026d59
Rohan <shingaderohan96@gmail.com>
2024-02-14 17:45:13 +0530

    video 18: express intro done

--- video18-Express-intro/index.js
+++ video18-Express-intro/index.js
@@ -4,23 +4,18 @@ const app = express();
 //get methods provide route
 
 //creating home page 
-app.get('', (req, res)=>{ // root page or home (by default)
-  res.send('<h1> Hello, this is Home page </h1>');
+app.get('',(req, res)=>{
+  res.send('<h1> Hello this is Home page </h1>')
 })
 
-//creating About page
-app.get('/about', (req, res)=>{
-  res.send('<h1> Hello, this is About page </h1>');
+//creating about page
+app.get('/about',(req, res)=>{
+  res.send('<h1> Hello this is About page </h1>')
 })
 
 //creating Help page
-app.get('/help', (req, res)=>{
-  res.send('<h1> Hello, this is gonna be Help page </h1>');
-})
-
-//Contact page
-app.get('/contact', (req, res)=>{
-  res.send('<h1> Hello, Contact US page </h1>');
+app.get('/help',(req, res)=>{
+  res.send('<h1> Help page </h1>')
 })
 
 app.listen(5000);
