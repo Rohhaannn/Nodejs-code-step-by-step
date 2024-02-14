9e7609d116be268ea76b5da53cb1c4b14b96344c
Rohan <shingaderohan96@gmail.com>
2024-02-14 18:43:52 +0530

    video 19: Routing Params - req & res done

--- video19-Routing-Params/index.js
+++ video19-Routing-Params/index.js
@@ -1,16 +1,24 @@
-/* 
-This is a Simple example of getting data from browser
 
-*/
+// This is a Simple example of getting data from browser
+
 
 const express = require("express");
 const app = express();
 
+/* 
+quick notes :
+
+request (req) & response (res) used to interact between server & client
+
+whenever I have to send or request some data, from the client browser, it will come into req paramerter. (jo bhi data client se aayega woh 'req' parameter mei store hoga)
+
+whatever data we are sending, we are sending it through 'res' parameter (jo bhi data hum send kr rahe hai, woh 'res' ke through send krte hai)
+
+*/
+
 app.get('', (req, res)  =>{
-  // console.log("data sent by browser =>>>", req.query)
   console.log("data sent by browser =>>>", req.query.name)
-  // res.send('Welcome, this is a demo page');
-  res.send('Welcome, '+ req.query.name);
+  res.send('Welcome, '+ req.query.name); // this will going to print on browser
 })
 
 app.listen(6500);
