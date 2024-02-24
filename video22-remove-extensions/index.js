93a24259667fceff927f13a34f452431c65c979c
Rohan <shingaderohan96@gmail.com>
2024-02-24 18:02:44 +0530

    video 22: remove extensions for URL completed

--- video22-remove-extensions/index.js
+++ video22-remove-extensions/index.js
@@ -2,38 +2,40 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const publicPath = path.join(__dirname, 'public');
+const publicPath = path.join(__dirname, "public");
+
 // app.use(express.static(publicPath));
 
-app.get('',(_, res)=>{
-  res.sendFile(`${publicPath}/home.html`);
+app.get('', (_, resp) => {
+  resp.sendFile(`${publicPath}/index.html`)
 })
 
-app.get('/about',(_, res)=>{
-  res.sendFile(`${publicPath}/about.html`);
+app.get('/home', (_, resp) => {
+  resp.sendFile(`${publicPath}/home.html`)
 })
 
-app.get('/help',(_, res)=>{
-  res.sendFile(`${publicPath}/help.html`);
+app.get('/about', (_, resp) => {
+  resp.sendFile(`${publicPath}/about.html`)
 })
 
-app.get('*',(_, res)=>{
-  res.sendFile(`${publicPath}/notFound.html`);
+app.get('*', (_, resp) => {
+  resp.sendFile(`${publicPath}/error.html`)
 })
 
-app.listen(8000);
+app.listen(4500);
 
 
 
 /* Interview Questions:
 
-Q. What is 404 page ?
+Q. what is 404 page ? how do we make them ?
 => 404 page is an error page or special kind of page when user enters a page to access which not exist at that time this page will showed up containing text "Error 404: Page not Found"
 
-Q. how to remove extensions from URL's to access / while accessing a web page ?
-=> using sendFile in get method
+Q. how to remove extensions from URL's to access / while accessing a web page ? OR
+Q. Q. How to remove extensions from URL ?
+=> res.sendFile(`${publicPath}/help.html`);
 
-Q. In Get Method how do you load a file ? 
-=> using sendFile Method
+Q. How do you load a file in Get method
+=> using sendFile()
 
 */
