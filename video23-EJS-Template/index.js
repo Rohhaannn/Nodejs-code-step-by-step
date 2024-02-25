35f39e6232b144ad594e8cbf93dae25c8e6d0e0e
Rohan <shingaderohan96@gmail.com>
2024-02-25 18:09:15 +0530

    video 23- Ejs template engine complete

--- video23-EJS-Template/index.js
+++ video23-EJS-Template/index.js
@@ -1,43 +1,38 @@
-const express = require('express');
-const path = require('path');
+// setting ejs be like
+// app.set('view engine', 'ejs'); this view engine & ejs are first & second parameter. and this is only valid syntax
+
+const express = require("express");
+const path = require("path");
 
 const app = express();
-const publicPath = path.join(__dirname, 'public');
+const publicPath = path.join(__dirname, "public");
 // app.use(express.static(publicPath));
 
-app.set('view engine','ejs');
+app.set("view engine", "ejs");
 
-// app.get('',(_, res)=>{
-//   res.sendFile(`${publicPath}/home.html`);
-// })
+// app.get("", (_, resp) => {
+//   resp.sendFile(`${publicPath}/index.html`);
+// });
 
-// app.get('/about',(_, res)=>{
-//   res.sendFile(`${publicPath}/about.html`);
-// })
-
-// app.get('/help',(_, res)=>{
-//   res.sendFile(`${publicPath}/help.html`);
-// })
-
-// app.get('*',(_, res)=>{
-//   res.sendFile(`${publicPath}/notFound.html`);
-// })
-
-app.get('/profile',(_, res)=>{
-  const user = { //this is the data we can pass dynamically
-    name: 'Ronak Sharma',
-    email: 'ronak@gmail.com',
-    country: 'India',
+app.get("/profile", (_, resp) => {
+  const user = { //this the way we can pass the data dynamically
+    name:'Ronak Sharma',
+    email:'ronak@gmail.com',
+    country:'India'
   }
-  res.render(`profile`,{user})
+  resp.render(`profile`, {user});
 });
 
-app.listen(8000);
+// app.get("/home", (_, resp) => {
+//   resp.sendFile(`${publicPath}/home.html`);
+// });
 
+// app.get("/about", (_, resp) => {
+//   resp.sendFile(`${publicPath}/about.html`);
+// });
 
-/* Interview Questions:
+// app.get("*", (_, resp) => {
+//   resp.sendFile(`${publicPath}/error.html`);
+// });
 
-
-
-
-*/
+app.listen(4500);
