13fc1ea61bb95fa38cb9b407b9e0a0bbb57750d1
Rohan <shingaderohan96@gmail.com>
2024-02-28 16:31:41 +0530

    mongoDB Installtion

--- video27-MongoDB/index.js
+++ video27-MongoDB/index.js
@@ -1,6 +1,13 @@
+/*
+MongoDb installation done
+
+*/
+
+
 /* Interview Questions: 
 
 Q. How do you setup environment for mondoDB ?
-Q. State a GUI name for MongoDB ? Ans: MongoDb Compass
+Q. State a GUI name for MongoDB ? 
+Ans: MongoDb Compass
 
 */
