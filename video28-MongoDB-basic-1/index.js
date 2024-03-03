4970fe8337ce5a4376d74d1cf69bbfcdeffc0245
Rohan <shingaderohan96@gmail.com>
2024-03-03 21:28:59 +0530

    video 28 mongo basic comnds done

--- video28-MongoDB-basic-1/index.js
+++ video28-MongoDB-basic-1/index.js
@@ -1,29 +1,22 @@
 /* 
 
-MongoDb basic :
-
-MongoDB is a noSQL database
-The data stored in a collection
-Collection don't have row & columns
-Data is sorted in the form of object.
-
-===========================================================
-
-Commands:
+MongoDb basic commands:
+=====================================
 
 to display present databases:
-show dbs
+=> show dbs
 
 ------------------------------------------------
 
 create new database:
 use newDatabaseName
->switched to db newDatabaseName
+=> switched to db newDatabaseName //output will look like this on cmd
 
 ------------------------------------------------
 
 create a new collection: (in new created db):
 db.createCollection('collectionName')
+< { ok : 1 }
 
 ------------------------------------------------
 
@@ -44,7 +37,13 @@ admin>
 ==================================================================
 
 //Interview Questions:
+
 Q. what is MongoDB ?
+=> MongoDB is a noSQL database
+=> The data stored in a collection
+=> Collection don't have row & columns
+=> Data is sorted in the form of object.
+
 Q. what is difference between SQL & MongoDB ?
 
 
